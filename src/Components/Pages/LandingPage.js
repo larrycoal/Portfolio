f858e6d6000e9b29c114c1b668c13f91bf904cf3
Olayiwola Olanrewaju <olayiwolaolanrewaju@Olayiwolas-MacBook-Pro.local>
2020-03-23 09:23:34 +0100

    responsiveness2

--- src/Components/Pages/LandingPage.js
+++ src/Components/Pages/LandingPage.js
@@ -5,12 +5,15 @@ const landingPage = () => {
     return (
         <div className="top-div">
             <div>
+            <div className="main-subject">
             <p>Hello,I am Lanre</p>
-            <h1>I AM A<span className=" animate"> </span></h1>
+            <h1>I AM A<span className="animate"> </span></h1>
+            </div>
             <div className="cv-btn">
                 <a href="https://res.cloudinary.com/dbdrtuscd/image/upload/v1584465373/Olayiwola_cv_txtmin.pdf">View cv</a>
             </div>
             </div>
+            
         </div>
     )
 }
