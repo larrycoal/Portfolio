ced0db5a9e65bdbac6a2f3cd2d4159499792c725
Olayiwola Olanrewaju <olayiwolaolanrewaju@Olayiwolas-MacBook-Pro.local>
2020-04-09 18:42:02 +0100

    we dey ontop am

--- src/Components/Pages/LandingPage.js
+++ src/Components/Pages/LandingPage.js
@@ -5,11 +5,11 @@ const landingPage = () => {
     return (
         <div className="top-div">
             <div className="landing-content">
-            <div className="main-subject">
-            <p>Hello,I am Lanre</p>
+            <div className="main-subject animated slideInLeft slow">
+            <p>Hello,</p>
             <h1>I AM A<span className="animate"> </span></h1>
             </div>
-            <div className="cv-btn">
+            <div className="cv-btn animated fadeInDown delay-2s">
                 <a href="https://res.cloudinary.com/dbdrtuscd/image/upload/v1584465373/Olayiwola_cv_txtmin.pdf">View cv</a>
             </div>
             </div>
